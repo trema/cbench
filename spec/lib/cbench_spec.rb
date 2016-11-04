1cd96c0da38a151fe97286eb29b1016faefa2a6f
Yasuhito Takamiya <yasuhito@gmail.com>
2016-11-04 17:29:28 +0900

    Refactoring

--- spec/lib/cbench_spec.rb
+++ spec/lib/cbench_spec.rb
@@ -12,34 +12,28 @@ describe Cbench do
   describe '#start' do
     context 'with []' do
       When { cbench.start([]) }
-      Then do
-        expect(logger).to(have_received(:info).with('Cbench started.'))
-      end
+      Then { expect(logger).to have_received(:info).with('Cbench started.') }
     end
   end
 
   describe '#packet_in' do
-    When { cbench.packet_in(dpid, message) }
+    When { cbench.packet_in(dpid, packet_in) }
 
-    context 'with 0xabc, PacketIn double' do
+    context 'with 0xabc, PacketIn (buffer_id: "BUFFER ID", in_port: 99)' do
       Given(:dpid) { 0xabc }
-      Given(:message) do
+      Given(:packet_in) do
         double('packet_in').tap do |packet_in|
           allow(packet_in).to receive(:buffer_id).and_return('BUFFER ID')
           allow(packet_in).to receive(:in_port).and_return(99)
         end
       end
-      Given do
-        allow(Pio::ExactMatch).to(receive(:new).with(message).
-                                  and_return('EXACT MATCH'))
-      end
+      Given { allow(Pio::ExactMatch).to receive(:new).with(packet_in).and_return('EXACT MATCH') }
       Given { allow(cbench).to receive(:send_flow_mod_add) }
       Then do
-        expect(cbench).to(have_received(:send_flow_mod_add).
-                          with(0xabc,
-                               match: 'EXACT MATCH',
-                               buffer_id: 'BUFFER ID',
-                               actions: Pio::SendOutPort.new(100)))
+        expect(cbench).to have_received(:send_flow_mod_add).with(0xabc,
+                                                                 match: 'EXACT MATCH',
+                                                                 buffer_id: 'BUFFER ID',
+                                                                 actions: Pio::SendOutPort.new(100))
       end
     end
   end
